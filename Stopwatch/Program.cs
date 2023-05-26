575d5768dd23713b3591e257cd826e78f9098b19
Pedro Lazari <67986647+PedroLazari233@users.noreply.github.com>
2023-05-25 22:46:28 -0300

    Full project

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Stopwatch
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            while (true)
+            {
+                Console.WriteLine("S to start, E to end and I for the interval");
+                var input = Console.ReadLine();
+
+                if(input == "S")
+                {
+                    stopwatch.StartWatch();
+                    Console.WriteLine(stopwatch.StartTime);
+                }
+                else if(input == "E")
+                {
+                    stopwatch.StopWatch();
+                    Console.WriteLine(stopwatch.EndTime);
+                }
+                else if(input == "I")
+                {
+                    stopwatch.ProvidePeriod();
+                    Console.WriteLine(String.Format("{0}", stopwatch.Interval));
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input");
+                    Console.WriteLine("S to start, E to end and P for the period");
+                }
+            }
+        }
+    }
+}
---
+++ Stopwatch/Stopwatch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    internal class Stopwatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+
+        public double Interval { get; set; }
+
+        public DateTime StartTime
+        {
+            get { return _startTime; }
+            set { _startTime = value; }
+        }
+        public DateTime EndTime
+        {
+            get { return _endTime; }
+            set { _endTime = value; }
+        }
+
+        public void StartWatch()
+        {
+            _startTime = DateTime.UtcNow;
+        }
+
+        public void StopWatch()
+        {
+            _endTime = DateTime.UtcNow;
+        }
+
+        public void ProvidePeriod()
+        {
+            Interval = (EndTime - StartTime).TotalSeconds;
+            _startTime = new DateTime(0);
+            _endTime = new DateTime(0);
+        }
+    }
+}
